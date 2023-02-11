ad7172a88e55a9611e467f2ea6d595ca183ae7bd
Aleff Soares <aleffsoares8@gmail.com>
2023-02-10 22:39:44 -0400

    Adicionado estilo

--- App.tsx
+++ App.tsx
@@ -1,7 +1,34 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet} from "react-native";
 
 export default function App(){
   return (
-    <Text> Aleff Soares </Text>
-  )
+    <View style={styles.container}>
+        <Text style={styles.eventName}> 
+          Nome do evento
+        </Text>
+
+        <Text style={styles.eventDate}> 
+          Sexta, 4 de Novembro de 2022.
+        </Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex:1,
+      backgroundColor: '#131016',
+      padding: 24,
+    },
+    eventName: {
+      color: '#FDFCFE',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 48,
+    },
+    eventDate: {
+      color: '#6B6B6B',
+      fontSize: 16,
+    },
+
+});

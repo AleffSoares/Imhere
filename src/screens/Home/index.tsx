96f04bc2687df803e8f49e5b7ddf7ef6f4b463b0
Aleff Soares <aleffsoares8@gmail.com>
2023-02-11 14:15:14 -0400

    Adicionado botão e evento no clique

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from './styles';
 
 
 export  function Home(){
+
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!");
+  }
   return (
     <View style={styles.container}>
         <Text style={styles.eventName}> 
@@ -18,8 +22,13 @@ export  function Home(){
           style={styles.input}
           placeholder="Nome do participante"  
           placeholderTextColor="#6B6B6B"
-          
         />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+           <Text style={styles.buttonText}>
+              +
+            </Text>
+        </TouchableOpacity>
     </View>
   );
 }

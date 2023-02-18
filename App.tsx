e324b2d2a6256a285f6a17264c2fdb8577bad7f0
Aleff Soares <aleffsoares8@gmail.com>
2023-02-18 17:09:44 -0400

    Adicionado o StatusBar

--- App.tsx
+++ App.tsx
@@ -3,7 +3,13 @@ import{ Home } from "./src/screens/Home";
 
 export default function App() {
   return (
-
-      <Home />
+    <>
+        <StatusBar 
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+        />
+        <Home />
+    </>
   );
 }
